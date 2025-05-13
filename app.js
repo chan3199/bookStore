e5b2be07e0d75b47958ba495d371cce17ac76393
Lee jeongchan <sjk338285@gmail.com>
2025-05-13 16:53:00 +0900

    create : users 라우터 추가

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const dotenv = require("dotenv");
 const express = require("express");
+const userRouter = require("./routes/users");
 const app = express();
 
 dotenv.config();
 
 app.listen(process.env.PORT);
+
+app.use("/", userRouter);

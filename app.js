7ac62259eb1ce669fa4f4707070772538b255361
Lee jeongchan <sjk338285@gmail.com>
2025-05-13 17:14:57 +0900

    create : api 모듈화 및 추가

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const dotenv = require("dotenv");
 const express = require("express");
-const userRouter = require("./routes/users");
 const app = express();
 
 dotenv.config();
 
 app.listen(process.env.PORT);
 
-app.use("/", userRouter);
+const userRouter = require("./routes/users");
+const bookRouter = require("./routes/users");
+const likeRouter = require("./routes/users");
+const cartRouter = require("./routes/users");
+const orderRouter = require("./routes/users");
+
+app.use("users/", userRouter);
+app.use("carts/", cartRouter);
+app.use("books/", bookRouter);
+app.use("orders/", orderRouter);
+app.use("likes/", likeRouter);
---
+++ routes/likes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+router.use(express.json());
+
+router.put("/", (req, res) => {
+  res.json("좋아요 추가");
+});
+
+router.delete("/", (req, res) => {
+  res.json("좋아요 취소소");
+});
+
+module.exports = router;

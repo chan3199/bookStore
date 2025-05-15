e6e594ab57fee76a0e163470801f433becf1863f
Lee jeongchan <sjk338285@gmail.com>
2025-05-15 17:12:25 +0900

    feat : 전체도서, 개별 도서 조회 API

--- controller/bookController.js
+++ controller/bookController.js
@@ -2,11 +2,30 @@ const conn = require("../mariadb");
 const { StatusCodes } = require("http-status-codes");
 
 const allBooks = (req, res) => {
-  res.json("전체 도서 조회");
+  const sql = "SELECT * FROM books";
+  conn.query(sql, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    res.status(StatusCodes.OK).json(results);
+  });
 };
 
 const bookDetail = (req, res) => {
-  res.json("개별별 도서 조회");
+  const { id } = req.params;
+  const sql = "SELECT * FROM books WHERE id=?";
+  conn.query(sql, id, (err, results) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    if (results[0]) {
+      return res.status(StatusCodes.OK).json(results);
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED).end();
+    }
+  });
 };
 
 const booksByCategory = (req, res) => {

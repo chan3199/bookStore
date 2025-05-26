7dbdad4744860c0e0cd5b203b2e676e4b03ad8b4
Lee jeongchan <sjk338285@gmail.com>
2025-05-26 21:43:04 +0900

    feat : 주문하기 API 구현 완료

--- controller/orderController.js
+++ controller/orderController.js
@@ -13,23 +13,30 @@ const order = async (req, res) => {
       firstBookTitle,
     } = req.body;
 
-    const [deliveryResult] = await conn.query(
+    const [deliveryResult] = await conn.execute(
       "INSERT INTO delivery (address, receiver, contact) VALUES (?, ?, ?)",
       [delivery.address, delivery.receiver, delivery.contact]
     );
     const deliveryId = deliveryResult.insertId;
 
-    const [orderResult] = await conn.query(
+    const [orderResult] = await conn.execute(
       "INSERT INTO orders (book_title, total_quantity, total_price, user_id, delivery_id) VALUES (?, ?, ?, ?, ?)",
       [firstBookTitle, totalQuantity, totalPrice, userId, deliveryId]
     );
     const orderId = orderResult.insertId;
 
-    const orderItems = items.map((i) => [orderId, i.bookId, i.quantity]);
+    const [cartItems, _] = await conn.query(
+      `SELECT book_id, quantity FROM cartItems WHERE id IN (?)`,
+      [items]
+    );
+
+    const orderItems = cartItems.map((i) => [orderId, i.book_id, i.quantity]);
     const [finalResult] = await conn.query(
       "INSERT INTO orderedBook (order_id, book_id, quantity) VALUES ?",
       [orderItems]
     );
+
+    deleteCartItems(conn, items);
 
     res.status(StatusCodes.OK).json(finalResult);
   } catch (err) {
@@ -38,6 +45,12 @@ const order = async (req, res) => {
   } finally {
     conn.end();
   }
+};
+
+const deleteCartItems = async (conn, items) => {
+  const sql = `DELETE FROM cartItems WHERE id IN (?)`;
+
+  return await conn.query(sql, [items]);
 };
 
 const getOrders = (req, res) => {
